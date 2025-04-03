b684b3a5ef316f9d5d4161fc17e51e2c6caa3228
elena <ecatelli@studenti.uninsubria.it>
2025-04-03 11:53:16 +0200

    Classe ristorante creata

--- src/Dominio/Ristorante.java
+++ src/Dominio/Ristorante.java
@@ -1,4 +1,52 @@
 package Dominio;
 
 public class Ristorante {
+    String nome;
+    String nazione;
+    String citta;
+    String indirizzo;
+    int fasciaPrezzo;
+    boolean servizioDelivery;
+    boolean servizioPrenotazioneOnline;
+    String tipoCucina;
+
+    Ristorante(String nome, String nazione, String citta, String indirizzo, int fasciaPrezzo, boolean servizioDelivery, boolean servizioPrenotazioneOnline, String tipoCucina){
+        this.nome = nome;
+        this.nazione = nazione;
+        this.citta = citta;
+        this.indirizzo = indirizzo;
+        this.fasciaPrezzo = fasciaPrezzo;
+        this.servizioDelivery = servizioDelivery;
+        this.servizioPrenotazioneOnline = servizioPrenotazioneOnline;
+        this.tipoCucina = tipoCucina;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public String getNazione(){
+        return nazione;
+    }
+    public String getCitta(){
+        return citta;
+    }
+    public String getIndirizzo(){
+        return indirizzo;
+    }
+    public int getFasciaPrezzo(){
+        return fasciaPrezzo;
+    }
+    public boolean getServizioDelivery(){
+        return servizioDelivery;
+    }
+    public boolean getServizioPrenotazioneOnline(){
+        return servizioPrenotazioneOnline;
+    }
+    public String getTipoCucina(){
+        return tipoCucina;
+    }
+
+
+
+
 }
