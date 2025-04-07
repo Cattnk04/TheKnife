f4c7802f4de62faf359b434d5b071286ac9b843f
Cattank04 <giacomo.tancredi06@gmail.com>
2025-04-07 10:53:48 +0200

    campi classe utente

--- src/Dominio/Utenti.java
+++ src/Dominio/Utenti.java
@@ -1,4 +1,46 @@
 package Dominio;
 
 public class Utenti {
+    private String nome;
+    private String cognome;
+    private String email;
+    private String username;
+    private String citta;
+    private boolean ristoratore;
+    private String passwordHash;
+
+    public String getNome(){
+        return  this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCognome(){
+        return  this.cognome;
+    }
+    public void setCognome(String cognome){
+        this.cognome = cognome;
+    }
+    public String getEmail(){
+        return  this.email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getUsername(){
+        return  this.username;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public String getCitta(){
+        return  this.citta;
+    }
+    public void setCitta(String citta){
+        this.citta = citta;
+    }
+    public boolean getRistoratore(){
+        return  this.ristoratore;
+    }
 }
