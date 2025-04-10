79fd91c4e24c24b960bb6ea51cdb9ef18526a64a
Cattank04 <giacomo.tancredi06@gmail.com>
2025-04-10 11:47:58 +0200

    controllo dei duplicati sia in Ristoranti che in Utenti rispettivamente in base al nome del ristorante e in base all'email dell'utente. queste due classi lanciano l'eccezione non gestita che va rimandata a quando vengono chiamate le due classi (dato che sono nel costruttore.

--- src/Dominio/Ristorante.java
+++ src/Dominio/Ristorante.java
@@ -1,24 +1,26 @@
 package Dominio;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class Ristorante {
-    private String nome;
+    private final String nome;
     private String nazione;
-    private String citta;
+    private String provincia;
     private String indirizzo;
-    private Integer fasciaPrezzo;
+    private final String emailPropreitario;
+    private Integer prezzoMedio;
     private boolean servizioDelivery;
     private boolean servizioPrenotazioneOnline;
     private String tipoCucina;
 
-    Ristorante(String nome, String nazione, String citta, String indirizzo, Integer fasciaPrezzo, boolean servizioDelivery, boolean servizioPrenotazioneOnline, String tipoCucina){
-        this.nome = nome;
-        this.nazione = nazione;
-        this.citta = citta;
-        this.indirizzo = indirizzo;
-        this.fasciaPrezzo = fasciaPrezzo;
+    Ristorante(String nome, String nazione, String provincia, String indirizzo, Integer fasciaPrezzo, String emailPropreitario,boolean servizioDelivery, boolean servizioPrenotazioneOnline, String tipoCucina) throws RuntimeException{
+        this.nome = nome.trim();
+        //check se esiste un altro ristorante con questo nome
+        controllaRistoranteDuplicato(nome);
+        this.nazione = nazione.trim();
+        this.provincia = provincia.trim();
+        this.indirizzo = indirizzo.trim();
+        this.prezzoMedio = fasciaPrezzo;
+        this.emailPropreitario = emailPropreitario.trim();
         this.servizioDelivery = servizioDelivery;
         this.servizioPrenotazioneOnline = servizioPrenotazioneOnline;
         this.tipoCucina = tipoCucina;
@@ -30,14 +32,17 @@ public class Ristorante {
     public String getNazione(){
         return this.nazione;
     }
-    public String getCitta(){
-        return this.citta;
+    public String getProvincia(){
+        return this.provincia;
     }
     public String getIndirizzo(){
         return this.indirizzo;
     }
-    public Integer getFasciaPrezzo(){
-        return this.fasciaPrezzo;
+    public String getEmailPropreitario(){
+        return this.emailPropreitario;
+    }
+    public Integer getPrezzoMedio(){
+        return this.prezzoMedio;
     }
     public boolean getServizioDelivery(){
         return this.servizioDelivery;
@@ -49,20 +54,17 @@ public class Ristorante {
         return this.tipoCucina;
     }
 
-    public void setNome(String nome){
-        this.nome = nome;
-    }
     public void setNazione(String nazione){
         this.nazione = nazione;
     }
-    public void setCitta(String citta){
-        this.citta = citta;
+    public void setProvincia(String provincia){
+        this.provincia = provincia;
     }
     public void setIndirizzo(String indirizzo){
         this.indirizzo = indirizzo;
     }
-    public void setFasciaPrezzo(Integer fasciaPrezzo){
-        this.fasciaPrezzo = fasciaPrezzo;
+    public void setPrezzoMedio(Integer prezzoMedio){
+        this.prezzoMedio = prezzoMedio;
     }
     public void setServizioDelivery(boolean servizioDelivery){
         this.servizioDelivery = servizioDelivery;
@@ -73,15 +75,34 @@ public class Ristorante {
     public void setTipoCucina(String tipoCucina){
         this.tipoCucina = tipoCucina;
     }
-    public void InserisciRistorante() {
+    public void salvaRistorante() {
         File fileRistoranti = new File("Ristoranti.txt");
 
         try {
             FileWriter writer = new FileWriter(fileRistoranti, true);
-            String stringa = this.nome.trim() + ',' + this.nazione.trim() + ',' + this.citta.trim() + ',' + this.indirizzo.trim() + ',' + this.fasciaPrezzo.toString() + ','+ this.servizioDelivery + ',' + this.servizioPrenotazioneOnline + ',' + this.tipoCucina.trim();
+            String stringa = this.nome + ',' + this.nazione + ',' + this.provincia + ',' + this.indirizzo + ',' + this.prezzoMedio.toString() + ','+ this.servizioDelivery + ',' + this.servizioPrenotazioneOnline + ',' + this.tipoCucina + ',' + this.emailPropreitario.trim();
             writer.write(stringa);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    private void controllaRistoranteDuplicato(String nomeRistorante) throws RuntimeException{
+        try{
+            File fileRistoranti = new File("Ristoranti.txt");
+            FileReader reader = new FileReader(fileRistoranti);
+            BufferedReader br = new BufferedReader(reader);
+            String stringa = br.readLine();
+            while(stringa!=null){
+                String[] arrayStringaRistorante = stringa.split(",");
+                if (arrayStringaRistorante[0].equals(nomeRistorante))
+                    throw new RuntimeException("Ristorante non puo' essere duplicato");
+                else
+                    stringa = br.readLine();
+            }
+        } catch(FileNotFoundException e){
+            System.out.println("File non trovato");
+        } catch(IOException e){
+            System.out.println("IO Exception");
         }
     }
 }
