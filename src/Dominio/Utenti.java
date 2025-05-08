4013e7c9ef22ee592c1f01b3638aaeae39f5a705
elena <ecatelli@studenti.uninsubria.it>
2025-05-08 10:20:28 +0200

    Utilizzo della classe Utenti alla fine della registrazione e costruttore nella classe Utenti

--- src/Dominio/Utenti.java
+++ src/Dominio/Utenti.java
@@ -1,14 +1,26 @@
 package Dominio;
 
+import java.io.*;
+
 public class Utenti {
+    public static final String FILE_UTENTI = "Utenti.txt";
     private String nome;
     private String cognome;
-    private String email;
-    private String username;
+    private final String email;
     private String provincia;
-    private boolean ristoratore;
+    private final boolean ristoratore;
     private String passwordHash;
 
+    public Utenti(String email,String nome,String cognome, String provincia, boolean ristoratore, String passwordHash) throws RuntimeException{
+        controllaUtenteDuplicato(email.trim());
+        this.email = email.trim();
+        controllaUtenteDuplicato(email);
+        this.nome = nome.trim();
+        this.cognome = cognome.trim();
+        this.provincia = provincia.trim();
+        this.ristoratore = ristoratore;
+        this.passwordHash = passwordHash;
+    }
     public String getNome(){
         return  this.nome;
     }
@@ -25,16 +37,40 @@ public class Utenti {
     public String getEmail(){
         return  this.email;
     }
-    public void setEmail(String email){
-        this.email = email;
-    }
-    public String getCitta(){
+    public String getProvincia(){
         return  this.provincia;
     }
-    public void setCitta(String provincia){
+    public void setProvincia(String provincia){
         this.provincia = provincia;
     }
     public boolean getRistoratore(){
         return  this.ristoratore;
+    }
+    public void controllaUtenteDuplicato(String email) throws RuntimeException{
+        try{
+            FileReader fileReader = new FileReader(FILE_UTENTI);
+            BufferedReader br = new BufferedReader(fileReader);
+            String stringa = br.readLine();
+            while(stringa!=null){
+                String[] arrayStringaUtente = stringa.split(",");
+                if(arrayStringaUtente[0].equals(email))
+                    throw new RuntimeException("Utente non puo' essere duplicato");
+                else
+                    stringa = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File non trovato");
+        } catch (IOException e) {
+            throw new RuntimeException("IO Exception");
+        }
+    }
+    public void salvaUtente(){
+        File fileUtenti = new File(FILE_UTENTI);
+        try{
+            FileWriter fileWriter = new FileWriter(fileUtenti, true);
+            String stringaUtente = this.email + ',' + this.nome + ',' + this.cognome + ',' + this.provincia + ',' + this.ristoratore + ',' + this.passwordHash;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
